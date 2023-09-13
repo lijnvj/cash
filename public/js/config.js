2a310de35cfa55370b01803994fcad8e461d1406
Yeah, I like it <128924097+lijnvj@users.noreply.github.com>
2023-09-14 03:26:37 +0800

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,24 @@
+var pay_list = [
+    {
+    'type':'qq',
+	'name':'QQ支付',
+	'text':'打开QQ扫一扫',
+	'qr':'./public/images/pay/new/qq.jpg',
+    'pass':'',
+  },
+  {
+    'type':'wechat',
+	'name':'微信支付',
+	'text':'打开微信宝扫一扫',
+	'qr':'./public/images/pay/new/weixin.jpg',
+    'pass':'',
+  },
+  {
+    'type':'alipay2',
+	'name':'支付宝支付',
+	'text':'打开支付宝扫一扫',
+	'qr':'./public/images/pay/new/zhifubao.jpg',
+    'pass':'',
+  },
+
+];
